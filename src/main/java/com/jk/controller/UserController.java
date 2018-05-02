9d92545acf179c3d0db83efcf8b429ebc96c47e1
sunbo <2663616534@qq.com>
2018-05-02 20:18:28 +0800

    第一次git上传

---
+++ src/main/java/com/jk/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.jk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import sun.applet.Main;
+@RestController
+public class UserController {
+
+
+}
